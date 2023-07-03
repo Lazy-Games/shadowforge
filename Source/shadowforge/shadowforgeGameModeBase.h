a7a49b6b8d5b9433cc306a365d359ed3b558c709
Carlos Suarez <kazh027@gmail.com>
2023-07-03 15:13:15 -0600

    Unreal 5 Blank Project Started

---
+++ Source/shadowforge/shadowforgeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "shadowforgeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOWFORGE_API AshadowforgeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
